87e110ec48017642ca018d60b93fef06982cd4b2
Kotecha Udit Hitendra <64374551+kirito-udit@users.noreply.github.com>
2020-08-03 20:14:17 +0530

    Add files via upload

---
+++ PRIME1.cpp
@@ -0,0 +1,68 @@
+/* QUESTION:
+https://w...content-available-to-author-only...j.com/problems/PRIME1/
+Peter wants to generate some prime numbers for his cryptosystem. Help him! Your task is to generate all prime numbers between two given numbers!
+
+Input
+The input begins with the number t of test cases in a single line (t<=10). In each of the next t lines there are two numbers m and n (1 <= m <= n <= 1000000000, n-m<=100000) separated by a space.
+
+Output
+For every test case print all prime numbers p such that m <= p <= n, one number per line, test cases separated by an empty line.
+
+Example
+Input:
+2
+1 10
+3 5
+
+Output:
+2
+3
+5
+7
+
+3
+5
+Warning: large Input/Output data, be careful with certain languages (though most should be OK if the algorithm is well designed)
+Information
+After cluster change, please consider PRINT as a more challenging problem.
+*/
+#include <iostream>
+using namespace std;
+bool is_prime(int n) {
+    // Assumes that n is a positive natural number
+    // We know 1 is not a prime number
+    if (n == 1) {
+        return false;
+    }
+
+    int i = 2;
+    // This will loop from 2 to int(sqrt(x))
+    while (i*i <= n) {
+        // Check if i divides x without leaving a remainder
+        if (n % i == 0) {
+            // This means that n has a factor in between 2 and sqrt(n)
+            // So it is not a prime number
+            return false;
+        }
+        i += 1;
+    }
+    // If we did not find any factor in the above loop,
+    // then n is a prime number
+    return true;
+}
+int main() {
+	
+	int T,m,n;
+	cin>>T;
+	while(T--)
+	{
+		cin>>m>>n;
+		for(int i=m;i<=n;i++)
+		{
+			if(is_prime(i))cout<<i<<endl;
+		}
+		cout<<endl;
+	}
+
+	return 0;
+}
